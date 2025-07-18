300134569d5fa86e43e7706357efab99bfe3ba32
Steven <stevenzhang.7161@gmail.com>
2025-07-17 22:45:34 -0700

    Dock Bar

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -1,12 +1,18 @@
 import Sidebar from '@/components/SideBar'
-export default function Home() {
+import DockBar from "@/components/DockBar";
+import { Dock, DockIcon } from "@/components/magicui/dock";
+
+
+export default function home() {
   return (
     <div className = 'grid md:grid-cols-2 grid-cols-1 min-h-screen'>
-      
+      <DockBar></DockBar>
+
       {/* left panel */}
       <div className="md:sticky top-0">
         <Sidebar></Sidebar>
       </div>
+
 
       {/* right panel*/}
       <div className = "overflow-y-auto h-screen">RIGHT PANEL </div>
