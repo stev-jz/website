4e0b121ef8124bb2fa71503ef1a55ee468fb42c5
Steven <stevenzhang.7161@gmail.com>
2025-07-21 13:11:38 -0700

    file capitalization

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -1,4 +1,4 @@
-import Sidebar from '@/components/SideBar'
+import Sidebar from '@/components/Sidebar'
 import { BackgroundGradientAnimation } from '@/components/ui/background-gradient-animation';
 import GlassContainer from '@/components/GlassContainer';
 import ExperienceSection from '@/components/sections/ExperienceSection';
