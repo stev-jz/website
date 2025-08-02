8257db1fa0184e993d26fbb049aa186cd22f5558
Steven <stevenzhang.7161@gmail.com>
2025-08-01 23:24:52 -0700

    minor components' changes

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,42 @@
+"use client";
+import ThemeToggle from "@/theme/theme-toggle";
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+
+export default function Navbar(){
+    const [scrolled, setScrolled] = useState(false);
+      useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 10);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+    return(
+        <nav 
+            className={`
+                group
+                w-full
+                fixed
+                h-[5vw]
+                z-10
+                transition duration-300 ease-in-out
+                backdrop-blur-xl
+                border-t border-t-white/30
+                flex gap-10 justify-center items-center p-6
+                ${scrolled ? "shadow-md dark:bg-black/20 bg-gray-600/15" : "bg-transparent"}
+                `}
+                >
+                <Link href='/'>
+                    <Image src="/s logo.png" alt="Home" width={40} height={40}/>
+                </Link>
+                <Link href="/">About</Link>
+                <Link href="/Experience">Experience</Link>
+                <Link href="/Projects">Projects</Link>
+                <ThemeToggle></ThemeToggle>
+        </nav>
+    );
+}
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,26 +1,27 @@
 import Link from 'next/link';
 import IconList from '@/components/Iconlist'
 import GlassContainer from './GlassContainer';
+import ThemeToggle from '@/theme/theme-toggle';
 
 export default function Sidebar(){
     return(
-        <aside className="md:px-[8vw] h-screen flex flex-col">
-            <div className='pt-[7vw]'>
-                <Link href = '/' className="text-[4vw] opacity-85 text-white font-bold tracking-tighter">
+        <aside className="md:px-[8vw] fixed h-screen flex flex-col">
+            <div className='pt-[9vw]'>
+                <p className="text-[4vw] opacity-85 dark:text-white font-bold tracking-tighter">
                 Hi, I&apos;m Steven👋
-                </Link>
-                <h2 className="opacity-85 tracking-tight text-white text-[1.2vw] font-[475] -mt-2.5">
+                </p>
+                <h2 className="opacity-85 tracking-tight dark:text-white text-[1.2vw] font-[475] -mt-2.5">
                     Studying <Link href="https://www.ece.utoronto.ca/" target="_blank" 
                     className="relative inline-block
                     after:content-[''] after:absolute after:bottom-0 after:left-0
-                    after:w-0 after:h-[2px] after:bg-gray-300
+                    after:w-0 after:h-[1.75px] dark:after:bg-gray-300 after:bg-black
                     after:transition-[width] after:duration-300
-                    hover:after:w-full"
+                    hover:after:w-full
+                    "
                     > ECE at the University of Toronto</Link>
                 </h2>
             </div>
             <div className="pt-[1vw]"><IconList></IconList></div>
-
         </aside>
     )
 }
