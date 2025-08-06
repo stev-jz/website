b8d237f101a974441186b607baf70e0ee09f7b00
Steven <stevenzhang.7161@gmail.com>
2025-08-05 23:47:20 -0700

    deleted pages, added sections

---
+++ src/app/sections/about.tsx
@@ -0,0 +1,9 @@
+export default function About(){
+    return(
+        <section id="about" className=" rounded-full container-type-inline">
+            <div className="size-[clamp(1rem,2.5cqw,3rem)] shadow-xl">
+                <p>placeholder</p>
+            </div>
+        </section>
+    );
+}
---
+++ src/app/sections/experience.tsx
@@ -0,0 +1,59 @@
+import GlassContainer from "@/components/GlassContainer"
+
+export default function Experience(){
+    return(
+        <section id="experience" className="w-full">
+            <GlassContainer 
+                date="2024 — 2025"
+                title="Test"
+                description="Work in Progress."
+                link="https://www.google.com/"
+                tags={["React", "TypeScript", "Next.js", "Tailwind", "CSS"]}>
+            </GlassContainer>
+            <GlassContainer 
+                date="2024 — 2025"
+                title="Test"
+                description="Work in Progress."
+                link="https://www.google.com/"
+                tags={["React", "TypeScript", "Next.js", "Tailwind", "CSS"]}>
+            </GlassContainer> 
+            <GlassContainer 
+                date="2024 — 2025"
+                title="Test"
+                description="Work in Progress."
+                link="https://www.google.com/"
+                tags={["React", "TypeScript", "Next.js", "Tailwind", "CSS"]}>
+            </GlassContainer> 
+            <GlassContainer 
+                date="2024 — 2025"
+                title="Test"
+                description="Work in Progress."
+                link="https://www.google.com/"
+                tags={["React", "TypeScript", "Next.js", "Tailwind", "CSS"]}>
+            </GlassContainer> 
+            <GlassContainer 
+                date="2024 — 2025"
+                title="Test"
+                description="Work in Progress."
+                link="https://www.google.com/"
+                tags={["React", "TypeScript", "Next.js", "Tailwind", "CSS"]}>
+            </GlassContainer> 
+            <GlassContainer 
+                date="2024 — 2025"
+                title="Test"
+                description="Work in Progress."
+                link="https://www.google.com/"
+                tags={["React", "TypeScript", "Next.js", "Tailwind", "CSS"]}>
+            </GlassContainer> 
+            <GlassContainer 
+                date="2024 — 2025"
+                title="Test"
+                description="Work in Progress."
+                link="https://www.google.com/"
+                tags={["React", "TypeScript", "Next.js", "Tailwind", "CSS"]}>
+            </GlassContainer> 
+
+
+        </section>
+    )
+}
