a0bd095e8458870028881eaa268491619bcb1426
Steven <stevenzhang.7161@gmail.com>
2025-08-06 23:25:04 -0700

    draggable bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,58 @@
-import GlassContainer from '@/components/GlassContainer';
+"use client";
 
-export default function home() {
+import RightPanel from "@/components/RightPanel";
+import Sidebar from "@/components/Sidebar";
+import ThemeToggle from "@/theme/theme-toggle";
+import { useCallback, useRef, useState } from "react";
+
+export default function Home() {
+
+  const [leftWidth, setLeftWidth] = useState(45);
+  const draggingRef = useRef(false);
+  const startXRef = useRef(0);
+  const startWidthRef = useRef(0)
+
+  const handleMouseDown = useCallback((e: React.MouseEvent<HTMLDivElement>) => {
+    draggingRef.current = true;
+    startXRef.current = e.clientX
+    startWidthRef.current = leftWidth;
+    const onMouseMove = (moveEvent: MouseEvent) => {
+      if (!draggingRef.current) return;
+      const currentX = moveEvent.clientX;
+      const deltaX = currentX - startXRef.current
+      const deltaPct = (deltaX/window.innerWidth) * 100
+      const newWidth = startWidthRef.current + deltaPct;
+      setLeftWidth(Math.min(53, Math.max(35, newWidth)));
+
+    }
+    const onMouseUp = () => {
+      draggingRef.current = false;
+      window.removeEventListener("mousemove", onMouseMove);
+      window.removeEventListener("mouseup", onMouseUp);
+    }
+
+    window.addEventListener("mousemove", onMouseMove);
+    window.addEventListener("mouseup", onMouseUp)
+  }, [leftWidth]);
+
   return (
-    <div className="mt-[9vw]">
-      <GlassContainer 
-        date="2024 — 2025"
-        title="About"
-        description="Work in Progress."
-        link="https://www.google.com/"/>
-        <GlassContainer 
-        date="2024 — 2025"
-        title="About"
-        description="Work in Progress."
-        link="https://www.google.com/"/>
-        <GlassContainer 
-        date="2024 — 2025"
-        title="About"
-        description="Work in Progress."
-        link="https://www.google.com/"/>
-        <GlassContainer 
-        date="2024 — 2025"
-        title="About"
-        description="Work in Progress."
-        link="https://www.google.com/"/><GlassContainer 
-        date="2024 — 2025"
-        title="About"
-        description="Work in Progress."
-        link="https://www.google.com/"/>
-        <GlassContainer 
-        date="2024 — 2025"
-        title="About"
-        description="Work in Progress."
-        link="https://www.google.com/"/>
+    <div className="flex flex-col md:flex-row h-screen">
+      <div style = {{ width: `${leftWidth}%`}} className="overflow-hidden select-none transiton-all duration-75">
+        <Sidebar/>
+      </div>
 
+      <div onMouseDown={handleMouseDown} className="
+        z-50 
+        w-[4px] md:w-[4px] 
+        transition-opacity duration-150 opacity-50 hover:opacity-100 
+        hidden md:block 
+        select-none 
+        bg-gray-300 cursor-ew-resize">
+      </div>
+
+      <div draggable={false} className="overflow-auto flex-1 select-none">
+        <RightPanel/>
+      </div>
       
     </div>
   );
