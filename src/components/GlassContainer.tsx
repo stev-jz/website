8eba85e10c0612f5b3943a9ba6fa761f2e834da2
Steven <stevenzhang.7161@gmail.com>
2025-08-22 22:04:08 -0700

    x

--- src/components/GlassContainer.tsx
+++ src/components/GlassContainer.tsx
@@ -130,7 +130,7 @@ export default function GlassContainer(props: GlassContainerProps) {
             {props.tags?.map(tag => (
               <li
                 key={tag}
-                className="inline-flex items-center lg:px-3 lg:py-1 lg:text-[clamp(8px,2.3vw,12px)]
+                className="inline-flex items-center lg:px-3 lg:py-1 lg:text-[clamp(8px,2.3vw,11px)]
                            max-lg:text-[1.2vw] max-lg:leading-tight max-lg:px-2.5 max-lg:py-1
                            font-semibold rounded-full bg-blue-950 text-blue-500"
               >
